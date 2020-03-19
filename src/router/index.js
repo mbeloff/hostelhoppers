e3b00c0f03de217d37c3f5ada16f3c97c9193b56
michael beloff <michael.a.beloff@gmail.com>
2020-03-19 11:54:40 +1000

    temp remove pay code

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Pay from '../views/Pay.vue'
+// import Pay from '../views/Pay.vue'
 
 Vue.use(VueRouter)
 
@@ -21,12 +21,12 @@ const routes = [
         '../views/Locations.vue'
       )
     }
-  },
-  {
-    path: '/pay',
-    name: 'Pay',
-    component: Pay
   }
+  // {
+  //   path: '/pay',
+  //   name: 'Pay',
+  //   component: Pay
+  // }
 ]
 
 const router = new VueRouter({

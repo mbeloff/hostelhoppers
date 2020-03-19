f746f12d8ae88179c42930b0ebc07f2b1ec6b6f2
michael beloff <michael.a.beloff@gmail.com>
2020-03-19 11:58:54 +1000

    terms styling

--- src/components/terms.vue
+++ src/components/terms.vue
@@ -2,52 +2,52 @@
   <div class="col text-muted small">
     <h6 class="mb-0">Terms & Conditions</h6>
     <hr class="mt-0" />
-    <ol style="columns: 2">
-      <li class="mr-5">
+    <ol>
+      <li>
         Pick up and drop off in any Campervan VIllage locations around Australia
         and New Zealand
       </li>
-      <li class="mr-5">
+      <li>
         250km a day included free with all hires. Extra kilometres charged at
         $0.20 per kilometre
       </li>
-      <li class="mr-5">
+      <li>
         Damage cover excludes driving while under the influence of drugs or
         alochol, falling asleep at the wheel and malicious damage by the hirer
       </li>
-      <li class="mr-5">
+      <li>
         Should there be a major incident (mechanical fault or crash) a
         replacement vehicle will be available at the closest Campervan Village
         location
       </li>
-      <li class="mr-5">
+      <li>
         Manual and automatic vehicles are available, damage cover for an
         automatic is $5/day extra
       </li>
-      <li class="mr-5">
+      <li>
         Vehicles come with 1/4 tank of fuel and must be returned with the same
         amount
       </li>
-      <li class="mr-5">
+      <li>
         Tyres and windscreen damage is included in damage cover. Underbody, side
         impact, front impact, rear impact and rollover damage are all included
       </li>
-      <li class="mr-5">Single vehicle damage included</li>
-      <li class="mr-5">
+      <li>Single vehicle damage included</li>
+      <li>
         Standard vehicle bond is $500, SUV bond is $1000. Bonds are payable by
         credit and debit card at the first day of hire and held til the end of
         the term of credit
       </li>
-      <li class="mr-5">
+      <li>
         Incident with another vehicle or single vehicle incident liability is
         one dollar
       </li>
-      <li class="mr-5">
+      <li>
         Any malicious damage is a criminal offence and must be reported to the
         police
       </li>
-      <li class="mr-5">No refunds will be given for credit not used</li>
-      <li class="mr-5">Toll fees icluded</li>
+      <li>No refunds will be given for credit not used</li>
+      <li>Toll fees icluded</li>
     </ol>
   </div>
 </template>
@@ -56,4 +56,15 @@
 export default {}
 </script>
 
-<style></style>
+<style scoped lang="scss">
+ol {
+  columns: 1;
+  padding-left: 1rem;
+  @media only screen and (min-width: 500px) {
+    columns: 2;
+    li {
+      margin-right: 2rem;
+    }
+  }
+}
+</style>
